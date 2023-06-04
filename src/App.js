25613220aa54c22ef4939992388dbd5f5a83eb33
Rafael Azriaiev <rafael.azrv@gmail.com>
2023-06-04 20:56:00 +0300

    #1 updated card to use images for Jack Queen And King cards

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import Deck from './components/Deck'; // make sure the path to the Deck componen
 function App() {
   return (
     <div >
-      <main className='test'>
-        <Card rank="A" suit="spades" isFaceUp={true} />
-        <Card rank="6" suit="hearts" isFaceUp={true} />
-        <Card rank="7" suit="hearts" isFaceUp={true} />
-        <Card rank="8" suit="hearts" isFaceUp={true} />
-        <Card rank="9" suit="diamonds" isFaceUp={true} />
-        <Card rank="10" suit="clubs" isFaceUp={true} />
-      </main>
-
       <div>
         <Deck />
       </div>
